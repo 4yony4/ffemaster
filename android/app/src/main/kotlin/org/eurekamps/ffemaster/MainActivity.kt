23d0b3870316e2b86ccdce24a0238664f41630bd
Eurekamps <4yony4@gmail.com>
2024-10-23 09:27:44 +0200

    Init commit

---
+++ android/app/src/main/kotlin/org/eurekamps/ffemaster/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.eurekamps.ffemaster
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Views/SplashView.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+
+  double dbPorcentaje=0.0;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    loading();
+  }
+
+  void loading() async{
+
+
+
+    while (dbPorcentaje <= 1.0) {
+      print('Valor actual: $dbPorcentaje');
+      setState(() {
+        dbPorcentaje += 0.05;
+      });
+      await Future.delayed(Duration(seconds: 1));  // Pausa entre incrementos
+    }
+
+    Navigator.of(context).pushNamed("/homeview");
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("BIENVENIDO A FFEMASTER"),
+        LinearProgressIndicator(value: dbPorcentaje,),
+        Text("${dbPorcentaje*100}%"),
+        const CircularProgressIndicator()
+      ],
+    );
+  }
+}
+
